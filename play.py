709dd39ea2dc1fa5cb5131f84e80b5b65b664989
benbenwu176 <ben@wulabs.com>
2025-08-30 17:14:51 -0500

    Fluidsynth starter code

---
+++ play.py
@@ -0,0 +1,20 @@
+import time
+import fluidsynth
+
+fs = fluidsynth.Synth(samplerate=48000, gain=0.5)
+fs.start(driver="wasapi")
+
+sfid = fs.sfload("piano.sf2")
+fs.program_select(0, sfid, 0, 0)
+
+vol = 127
+chord = [60, 64, 67]
+
+for i in range(10):
+    for note in chord:
+        fs.noteon(0, note - 12, vol)
+    time.sleep(0.5)
+
+time.sleep(5.0)
+
+fs.delete()
