f7da260ff3af44ba0160ed783c823e87a04cf777
benbenwu176 <ben@wulabs.com>
2025-08-30 17:32:10 -0500

    Fluidsynth usb & refactor with music

--- code/play.py
+++ code/play.py
@@ -4,7 +4,7 @@ import fluidsynth
 fs = fluidsynth.Synth(samplerate=48000, gain=0.5)
 fs.start(driver="wasapi")
 
-sfid = fs.sfload("piano.sf2")
+sfid = fs.sfload("usb.sf2")
 fs.program_select(0, sfid, 0, 0)
 
 vol = 127
@@ -12,7 +12,7 @@ chord = [60, 64, 67]
 
 for i in range(10):
     for note in chord:
-        fs.noteon(0, note - 12, vol)
+        fs.noteon(0, note, vol)
     time.sleep(0.5)
 
 time.sleep(5.0)
